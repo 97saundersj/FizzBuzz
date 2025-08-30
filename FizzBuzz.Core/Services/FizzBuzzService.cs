2745e61c8222ddf6648497cd41268caad6307705
97saundersj <jacques_s@hotmail.co.uk>
2025-08-30 20:27:13 +0100

    Implimented Fizzbuzzservice and used in program

--- FizzBuzz.Core/Services/FizzBuzzService.cs
+++ FizzBuzz.Core/Services/FizzBuzzService.cs
@@ -4,12 +4,27 @@
     {
         public string[] GetFizzBuzzRange(int start, int end)
         {
-            throw new NotImplementedException();
+            if (start > end)
+                throw new ArgumentException("Start must be less than or equal to end");
+
+            var results = new string[end - start + 1];
+            for (int i = 0; i < results.Length; i++)
+            {
+                results[i] = GetFizzBuzzResult(start + i);
+            }
+            return results;
         }
 
         public string GetFizzBuzzResult(int number)
         {
-            throw new NotImplementedException();
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else
+                return number.ToString();
         }
     }
 }
