a26918c63cfa3c9b9101f5be318fc7d06053c27f
97saundersj <jacques_s@hotmail.co.uk>
2025-08-30 20:05:25 +0100

    Added initial fizzbuzz service and tests

---
+++ FizzBuzz.Core/Services/FizzBuzzService.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzz.Core.Services
+{
+    public class FizzBuzzService : IFizzBuzzService
+    {
+        public string[] GetFizzBuzzRange(int start, int end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFizzBuzzResult(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FizzBuzzTests/Services/FizzBuzzServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using FizzBuzz.Core.Services;
+
+namespace FizzBuzzTests.Services
+{
+    [TestClass()]
+    public class FizzBuzzServiceTests
+    {
+        [TestMethod]
+        [DataRow(1, "1")]
+        [DataRow(3, "Fizz")]
+        [DataRow(5, "Buzz")]
+        [DataRow(15, "FizzBuzz")]
+        public void GetFizzBuzzResult_ReturnsCorrectResult(int number, string expected)
+        {
+            // Arrange
+            var fizzBuzzService = new FizzBuzzService();
+
+            // Act
+            string result = fizzBuzzService.GetFizzBuzzResult(number);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [DataRow(1, 5, new string[] { "1", "2", "Fizz", "4", "Buzz" })]
+        [DataRow(10, 15, new string[] { "Buzz", "11", "Fizz", "13", "14", "FizzBuzz" })]
+        public void GetFizzBuzzRange_ReturnsCorrectSequence(int start, int end, string[] expected)
+        {
+            // Arrange
+            var fizzBuzzService = new FizzBuzzService();
+
+            // Act
+            string[] result = fizzBuzzService.GetFizzBuzzRange(start, end);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
