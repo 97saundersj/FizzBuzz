28a8f1622e75ce5deef03ecb5263190e35456bb3
97saundersj <jacques_s@hotmail.co.uk>
2025-09-01 20:16:21 +0100

    Changed Exsisting service tests to integration tests

--- FizzBuzz/ServiceConfiguration.cs
+++ FizzBuzz/ServiceConfiguration.cs
@@ -1,20 +1,24 @@
+using FizzBuzz.Core.Rules;
 using FizzBuzz.Core.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FizzBuzz
 {
-    internal static class ServiceConfiguration
+    public static class ServiceConfiguration
     {
         public static IServiceProvider ConfigureServices()
         {
             var services = new ServiceCollection();
-            
+
+            // Register the rule factory
+            services.AddScoped<IFizzBuzzRuleFactory, FizzBuzzRuleFactory>();
+
             // Register the FizzBuzz service
             services.AddScoped<IFizzBuzzService, FizzBuzzService>();
-            
+
             // Register the FizzBuzzRunner
             services.AddScoped<FizzBuzzRunner>();
-            
+
             // Build and return the service provider
             return services.BuildServiceProvider();
         }
